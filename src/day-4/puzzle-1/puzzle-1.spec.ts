fca9d369386cece0307c3cf7775baf8289f281d2
alexboisseau <alex.boisseau79@gmail.com>
2023-12-04 10:17:12 +0100

    day 4 puzzle 1

---
+++ src/day-4/puzzle-1/puzzle-1.spec.ts
@@ -0,0 +1,60 @@
+import { Day4Puzzle1 } from "./puzzle-1";
+import { input as adventOfCodeInput } from "./input";
+
+describe("Day 4 Puzzle 1", () => {
+  describe("One game", () => {
+    it("should return 1 with one winning number", () => {
+      const input = "Card   1: 42  3 | 42";
+      const result = Day4Puzzle1(input);
+
+      expect(result).toEqual(1);
+    });
+
+    it("should return 2 with two winning numbers", () => {
+      const input = "Card   1: 42  3 | 42  3";
+      const result = Day4Puzzle1(input);
+
+      expect(result).toEqual(2);
+    });
+
+    it("should return 4 with three winning numbers", () => {
+      const input = "Card   1: 42  3 10 5 | 42  3  5  8";
+      const result = Day4Puzzle1(input);
+
+      expect(result).toEqual(4);
+    });
+  });
+
+  describe("Two games", () => {
+    it("should return 2 with two games with one winning number", () => {
+      const input = `Card   1: 42  3 | 42
+      Card   1: 42  3 | 42`;
+      const result = Day4Puzzle1(input);
+
+      expect(result).toEqual(2);
+    });
+
+    it("should return 4 with two games with two winning numbers", () => {
+      const input = `Card   1: 42  3 | 42  3
+      Card   1: 42  3 | 42  3`;
+      const result = Day4Puzzle1(input);
+
+      expect(result).toEqual(4);
+    });
+
+    it("should return 6 with two games, one with with two winning numbers and ne with with three winning numbers", () => {
+      const input = `Card   1: 42  3 | 42  3
+      Card   1: 42  3 10 | 42  3 10`;
+      const result = Day4Puzzle1(input);
+
+      expect(result).toEqual(6);
+    });
+  });
+
+  describe("with advent of code input", () => {
+    it("log result for advent of code", () => {
+      const result = Day4Puzzle1(adventOfCodeInput);
+      console.log(`Day 4 Puzzle 1 : ${result}`);
+    });
+  });
+});
---
+++ src/day-4/puzzle-1/puzzle-1.ts
@@ -0,0 +1,54 @@
+type ScratchCardInformation = [number[], number[]];
+
+export function Day4Puzzle1(input: string): number {
+  const scratchCardsInformation = getScratchCardsInformation(input);
+  console.log(scratchCardsInformation);
+
+  const scores = scratchCardsInformation.map((i) => {
+    return getScratchCardScore(i);
+  });
+
+  return scores.reduce((acc, s) => acc + s, 0);
+}
+
+function getScratchCardsInformation(input: string): ScratchCardInformation[] {
+  const information: ScratchCardInformation[] = [];
+
+  input.split("\n").forEach((line) => {
+    const numbers = line.split(":")[1];
+
+    const winningNumbers = numbers
+      .split("|")[0]
+      .split(" ")
+      .filter((v) => v !== "")
+      .map((v) => parseInt(v));
+
+    const ourNumbers = numbers
+      .split("|")[1]
+      .split(" ")
+      .filter((v) => v !== "")
+      .map((v) => parseInt(v));
+
+    information.push([winningNumbers, ourNumbers]);
+  });
+
+  return information;
+}
+
+function getScratchCardScore(scratchCardInformation: ScratchCardInformation) {
+  const [winningNumbers, ourNumbers] = scratchCardInformation;
+
+  return ourNumbers.reduce(
+    (acc, n) => {
+      if (winningNumbers.includes(n)) {
+        if (acc.counter === 0) acc.score = 1;
+        else acc.score = acc.score * 2;
+
+        acc.counter = acc.counter + 1;
+      }
+
+      return acc;
+    },
+    { score: 0, counter: 0 }
+  ).score;
+}
